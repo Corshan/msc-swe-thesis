e0e19b3ec8d92ce41ae5213713c09fbcde8ed891
Corey Shanahan <coreyshanahan01@gmail.com>
2026-07-22 22:45:05 +0100

    [dev] updated output paths to better organise the files

--- tool/arch_recovery/paths.py
+++ tool/arch_recovery/paths.py
@@ -6,17 +6,22 @@ class ProjectPaths:
     root: Path
     src: Path
     instrumented: Path
+    output_dir: Path
     trace_dir: Path
+    diagrams_dir: Path
     trace_file: Path
 
     @classmethod
     def from_root(self, project_path: str, project_src_path: str) -> 'ProjectPaths':
         root = Path(project_path)
+        output_dir = root / "recon_data"
         return self(
             root=root,
             src=Path(project_src_path),
             instrumented=Path(f"{root}_instrumented"),
-            trace_dir=root / "recon_data",
-            trace_file=root / "recon_data" / "out.trace"
+            output_dir=output_dir,
+            trace_dir=output_dir / "traces",
+            diagrams_dir=output_dir / "diagrams",
+            trace_file=output_dir / "traces" / "out.trace"
         )
     
